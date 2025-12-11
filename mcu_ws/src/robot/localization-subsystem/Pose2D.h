f194fe4734c4cac671aa88ff053af05ff6c5dbdf
tpcannon7 <trevpcannon@gmail.com>
2025-12-11 14:02:40 -0500

    feat: added yaw calculation to IMU, expanded localization stuff

--- mcu_ws/src/robot/localization-subsystem/Pose2D.h
+++ mcu_ws/src/robot/localization-subsystem/Pose2D.h
@@ -1,3 +1,10 @@
+/**
+ * @file Pose2D.h
+ * @author Trevor Cannon
+ * @brief Pose2D object to represent objects in 2D space
+ * @date 12/11/2025
+ */
+
 #ifndef POSE2D_H
 #define POSE2D_H
 
@@ -15,9 +22,9 @@ class Pose2D {
   Pose2D(float _x = 0, float _y = 0, float _theta = 0)
       : x(_x), y(_y), theta(_theta) {};
 
-  void normalize();
-  void add();
-  void rotate();
+  void normalizeTheta();
+  void add(const Pose2D& pose);
+  void rotate(float angle);
 
  private:
 };

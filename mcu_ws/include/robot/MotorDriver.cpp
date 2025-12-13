6690b2abc6e51cdaa76192e55cae9da2c3e8cbb9
tpcannon7 <trevpcannon@gmail.com>
2025-12-13 13:20:16 -0500

    feat: motor driver finished?

--- mcu_ws/include/robot/MotorDriver.cpp
+++ mcu_ws/include/robot/MotorDriver.cpp
@@ -2,10 +2,40 @@
 
 namespace Drivers {
 
-bool MotorDriver::init() {}
+bool MotorDriver::init() {
+  initSuccess_ = true;
 
-void MotorDriver::update() {}
+  if (setup_.pwm_pin >= 0) {
+    // analogWriteFrequency(setup_.pwm_pin, 18310.55);
+    pinMode(setup_.pwm_pin, OUTPUT);
+  } else {
+    initSuccess_ = false;
+  }
 
-char* MotorDriver::getInfo() {}
+  if (setup_.dir_pin >= 0) {
+    pinMode(setup_.dir_pin, OUTPUT);
+  } else {
+    initSuccess_ = false;
+  }
+
+  return initSuccess_;
+}
+
+void MotorDriver::setPWM(int speed) {
+  pwmOut = map(abs(speed), 0, SPEED_MAX, 0, PWM_MAX);
+  pwmOut = constrain(pwmOut, 0, 255);
+  motorDirection = (speed >= 0);
+}
+
+void MotorDriver::update() {
+  digitalWrite(setup_.dir_pin, motorDirection);
+  analogWrite(setup_.pwm_pin, pwmOut);
+}
+
+char* MotorDriver::getInfo() {
+  snprintf(infoBuffer_, sizeof(infoBuffer_), "Motor: %s, PWM: %d, Dir: %d",
+           setup_.getId(), pwmOut, motorDirection);
+  return infoBuffer_;
+}
 
 }  // namespace Drivers
--- mcu_ws/include/robot/MotorDriver.h
+++ mcu_ws/include/robot/MotorDriver.h
@@ -8,24 +8,30 @@
 #ifndef MOTORDRIVER_H
 #define MOTORDRIVER_H
 
+#include <Arduino.h>
 #include <BaseDriver.h>
 
 namespace Drivers {
 
 class MotorDriverSetup : public Classes::BaseSetup {
  public:
-  int pwm;
+  const int pwm_pin;
+  const int dir_pin;
 
   ~MotorDriverSetup() = default;
   MotorDriverSetup() = delete;
 
-  MotorDriverSetup(char* _id) : Classes::BaseSetup(_id) {};
+  MotorDriverSetup(const char* _id, const int _pwm_pin, const int _dir_pin)
+      : Classes::BaseSetup(_id), pwm_pin(_pwm_pin), dir_pin(_dir_pin) {};
 
  private:
 };
 
 class MotorDriver : public Classes::BaseDriver {
  public:
+  const int PWM_MAX = 255;
+  const int SPEED_MAX = 255;
+
   ~MotorDriver() override = default;
   MotorDriver(const MotorDriverSetup& setup)
       : setup_(setup), BaseDriver(setup) {};
@@ -34,8 +40,13 @@ class MotorDriver : public Classes::BaseDriver {
   void update() override;
   char* getInfo() override;
 
+  void setPWM(int speed);
+
  private:
   const MotorDriverSetup setup_;
+  int pwmOut;
+  bool motorDirection;
+  char infoBuffer_[64];
 };
 }  // namespace Drivers
 

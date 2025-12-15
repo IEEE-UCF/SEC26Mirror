57561b750a427492f42833945187ef9fdff8b50d
tpcannon7 <trevpcannon@gmail.com>
2025-12-14 21:31:23 -0500

    feat: vectors

---
+++ mcu_ws/src/robot/drive-base/RobotDriveBase.h
@@ -0,0 +1,20 @@
+/**
+ * @file RobotDriveBase.h
+ * @author Trevor Cannon
+ * @brief MCU Drive Base
+ * @date 12/14/2025
+ */
+
+#ifndef ROBOTDRIVEBASE_H
+#define ROBOTDRIVEBASE_H
+
+#include "Localization.h"
+#include "MotorDriver.h"
+#include "Vector2D.h"
+
+class RobotDriveBase {
+ public:
+ private:
+};
+
+#endif

5e937497d6ed75ae03652497f61157d7e173741f
Syndric <65517819+syndric@users.noreply.github.com>
2025-12-16 14:42:05 -0500

    feat: add mcu and manager subsystem(?)

---
+++ mcu_ws/src/robot/machines/RobotLogic.h
@@ -0,0 +1,17 @@
+/**
+ * @file RobotLogic.h
+ * @date 12/16/25
+ * @author Aldem Pido
+ * @brief Main logic for the SEC26 robot.
+ */
+
+// ==========================================================
+// ## 1. Instantiate All Driver and Manager Objects ##
+// ==========================================================
+
+namespace Robot {
+
+};
+// --- Drivers ---
+
+// --- Subsystems ---

4e267340d3c2e0ef416dea2f32d3f21b6b4d2b47
Tealimes <alexpeacock56ten@gmail.com>
2025-12-15 21:53:28 -0500

    feat: finish generic analog read and implement it

--- mcu_ws/lib/robot/analogRead.h
+++ mcu_ws/lib/robot/analogRead.h
@@ -4,9 +4,10 @@
  * @brief Defines the Driver for generic analog read implementation
  * @date 12/15/2025
  */
+#include <Arduino.h>
 
-namespace Drivers{
+namespace Drivers {
 
-    
+int genAnalogRead(uint8_t pin);
 
-}
+}  // namespace Drivers

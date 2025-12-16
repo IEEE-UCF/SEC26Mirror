620e0869d947528f19f92d49538986f263c632e7
tpcannon7 <trevpcannon@gmail.com>
2025-12-15 21:08:59 -0500

    feat: first iteration finished

--- mcu_ws/src/robot/drive-base/localization-subsystem/Localization.h
+++ mcu_ws/src/robot/drive-base/localization-subsystem/Localization.h
@@ -17,7 +17,9 @@ using namespace RobotConfig;  // maybe change this im lazy
 class Localization {
  public:
   ~Localization() = default;
-  Localization() : currentPose(START_X, START_Y, START_THETA) {};
+  Localization(float _x = START_X, float _y = START_Y,
+               float _theta = START_THETA)
+      : currentPose(_x, _y, _theta) {};
 
   Pose2D getPose() { return currentPose; }
   void update(long leftTicks, long rightTicks, float yaw);

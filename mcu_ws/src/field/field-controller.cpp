9ba24a77d19a37fd0fca7e3754e64e7c58a90915
Tealimes <alexpeacock56ten@gmail.com>
2025-12-17 16:11:41 -0500

    feat: add initial main controller logic

--- mcu_ws/src/field/field-controller.cpp
+++ mcu_ws/src/field/field-controller.cpp
@@ -1,9 +1,18 @@
-/** Placeholder file */
-#include <Arduino.h>
-void setup() {
+#include <controller.h>
 
-}
+uint8_t buttonPin = 17;  // placeholder
+
+// controller configuration
+Field::ControllerSetup setupController =
+    Field::ControllerSetup("controller", buttonPin);
+Field::ControllerDriver Controller1 = Field::ControllerDriver(setupController);
+
+void setup() { Controller1.init(); }
 
 void loop() {
-    
+  Controller1.update();
+
+  if (Controller1.getStatus()) {
+    Serial.println("All tasks finished");
+  }
 }

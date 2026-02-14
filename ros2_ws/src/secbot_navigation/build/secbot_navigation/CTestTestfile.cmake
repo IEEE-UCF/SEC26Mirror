a369f2d704d944362e8f8ad34926b1c709ecf652
tpcannon7 <trevpcannon@gmail.com>
2026-02-13 23:13:16 -0500

    chore(nav): remove build and install artifacts from version control

--- ros2_ws/src/secbot_navigation/build/secbot_navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/ros2_workspaces/src/sec26ros/secbot_navigation
-# Build directory: /home/ubuntu/ros2_workspaces/src/sec26ros/secbot_navigation/build/secbot_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

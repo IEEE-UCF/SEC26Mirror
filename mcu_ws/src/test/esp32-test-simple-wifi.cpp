cc7cd7144d3e0d10047f4977b22753bdce4bd518
Gitea Actions <action@gitea.local>
2026-02-19 05:47:36 +0000

    style: auto-format code with clang-format

--- mcu_ws/src/test/esp32-test-simple-wifi.cpp
+++ mcu_ws/src/test/esp32-test-simple-wifi.cpp
@@ -9,11 +9,11 @@ void setup() {
     delay(1000);
     // This will print a number (1, 4, 5, or 6)
     Serial.print("Status: ");
-    Serial.println(WiFi.status()); 
+    Serial.println(WiFi.status());
   }
 }
 
 void loop() {
-    Serial.print("Main loop \n");
-    delay(1000);
+  Serial.print("Main loop \n");
+  delay(1000);
 }

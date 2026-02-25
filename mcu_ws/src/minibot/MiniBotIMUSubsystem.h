314d080240a1bdf870fb5cfdc8bd1ea96d62c6e7
tpcannon7 <trevpcannon@gmail.com>
2026-02-24 23:08:21 -0500

    feat: imu subsystem + mpu driver

---
+++ mcu_ws/src/minibot/MiniBotIMUSubsystem.h
@@ -0,0 +1,70 @@
+/**
+ * @file MiniBotIMUSubsystem.h
+ * @brief IMU subsystem for MiniBot using MPU6050
+ * @author Trevor Cannon
+ * @date 2/17/2026
+ */
+
+#pragma once
+
+#include <BaseSubsystem.h>
+#include <TimedSubsystem.h>
+#include <micro_ros_utilities/type_utilities.h>
+#include <microros_manager_robot.h>
+
+#include "MPU6050.h"
+
+namespace Subsystem {
+
+class MiniBotIMUSubsystemSetup : public Classes::BaseSetup {
+ public:
+  Drivers::MPU6050DriverSetup mpuSetup_;
+
+  MiniBotIMUSubsystemSetup() = delete;
+  MiniBotIMUSubsystemSetup(const char* _id,
+                           const Drivers::MPU6050DriverSetup& _mpuSetup)
+      : Classes::BaseSetup(_id), mpuSetup_(_mpuSetup) {};
+}
+
+class MiniBotIMUSubsystem : public IMicroRosParticipant,
+                            public Subsystem::TimedSubsystem {
+ public:
+  explicit MiniBotIMUSubsystem(const MiniBotIMUSubsystemSetup& setup)
+      : Subsystem::TimeSubsystem(setup), setup_(setup), mpu_(mpuSetup_) {};
+
+  void begin() override;
+  void update() override;
+  void pause() override;
+  void reset() override;
+  const char* getInfo() override;
+
+  bool onCreate(rcl_node_t* node, rclc_executor_t* executor) override;
+  void onDestroy() override;
+
+  void publishData() override;
+
+ private:
+  void calibrate();
+
+  const int CALIBRATE_SAMPLES = 100;
+  bool calibrated_ = false;
+
+  float a_x_off_ = 0.0f;
+  float a_y_off_ = 0.0f;
+  float a_z_off_ = 0.0f;
+
+  float g_x_off_ = 0.0f;
+  float g_y_off_ = 0.0f;
+  float g_z_off_ = 0.0f;
+
+  const MiniBotIMUSubsystemSetup setup_;
+  Drivers::MPU6050Driver mpu_;
+  Drivers::MPU6050DriverData data_;
+
+  rcl_node_t* node_ = nullptr;
+  rclc_executor_t* executor_ = nullptr;
+  rcl_publisher_t mpu_pub_;
+
+  sensor_msgs__msg__Imu mpu_msg_;
+}
+}  // namespace Subsystem
